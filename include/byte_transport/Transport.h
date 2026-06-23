cb6827dd393b53e27f05c5a1271603627675652e
Bayu K. Luknanto <bayu.luknanto@tii.ae>
2026-06-23 13:52:14 +0400

    Transport uses inline constructor instead of definition on cpp file

--- include/byte_transport/Transport.h
+++ include/byte_transport/Transport.h
@@ -6,8 +6,8 @@ namespace pendarlab::lib::comm::byte_transport
   class Transport
   {
   public:
-    Transport();
-    Transport(const Config& config);
+    Transport(){};
+    Transport(const Config& config){};
     virtual ~Transport() = default;
     virtual int read(unsigned char* buf, unsigned int buf_size) = 0;
     virtual int write(const unsigned char* buf, unsigned int length) = 0;
