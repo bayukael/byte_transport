4e591bd0359140cb4f5ae8be858e6ba7b2a8bb37
Bayu K. Luknanto <bayu.luknanto@tii.ae>
2026-06-26 14:41:38 +0400

    Rename config variable name

--- include/byte_transport/ConfigParseResult.h
+++ include/byte_transport/ConfigParseResult.h
@@ -9,9 +9,9 @@
 namespace pendarlab::lib::comm::byte_transport
 {
   struct ConfigParseResult {
-    std::optional<Config> value;
+    std::optional<Config> config;
     std::vector<std::string> messages;
 
-    bool ok() const { return value.has_value(); }
+    bool ok() const { return config.has_value(); }
   };
 } // namespace pendarlab::lib::comm::byte_transport
